8811c1e45914b63a68b22bf83dbb6ede408fdc1a
alessandro.verrecchia <alessandro.verrecchia@quebecormedia.com>
2020-12-12 21:08:59 -0500

    day 6

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -1,15 +1,36 @@
 package day6
 
-import readFileAsLinesUsingUseLines
+import java.io.File
 
 
 class Day6 {
     fun execute() {
-
         println("day 6")
-        val list = readFileAsLinesUsingUseLines("day6.txt")
 
+        var count = 0
+        var charIsPresent = true
+        var list = listOf<String>()
+
+        getList().forEach {
+            list = it.split("\r\n")
+            for (i in list[0].indices) {
+                charIsPresent = true
+                for (j in 1..list.size) {
+                    try {
+                        if (list[j].contains(list[0][i]).not()) {
+                            charIsPresent = false
+                        }
+                    } catch (e: Exception) {
+                    }
+                }
+                if (charIsPresent)
+                    count++
+            }
+        }
+        println(count)
     }
 
-
+    private fun getList(): List<String> {
+        return File("day6.txt").readText(Charsets.UTF_8).split("\r\n\r\n")
+    }
 }
